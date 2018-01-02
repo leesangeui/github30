ac9c15c5eb1dac7aed69c67329270b0e85480536
lsy05 <lsy05@DESKTOP-NBIRV0M>
2018-01-02 12:34:25 +0900

    A사용자 커밋~~~

--- JAVA.GIT/src/test.java
+++ JAVA.GIT/src/test.java
@@ -2,6 +2,7 @@
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("Btest Ä¿¹Ô~~~~~~~~~~");
+		System.out.println("Btest Ã¹¹øÂ°Ä¿¹Ô~~~~~~~~~~");
+		System.out.println("Btest µÎ¹øÂ°Ä¿¹Ô~~~~~~~~~~");
 	}
 }
